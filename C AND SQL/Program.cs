918b405554f49c3c4d37c7bf608c7a28aab38131
Irina <ten7473@mail.ru>
2021-05-30 12:39:12 +0500

    init projekt

---
+++ C AND SQL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_AND_SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
